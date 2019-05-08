128ddd93d2bd655e1681713aaa9dcc0fa32300c9
Tanzim Mokammel <mtanzim@gmail.com>
2019-05-08 12:39:00 +0200

    finish week 3 assn

--- src/module3/EarthquakeCityMap.java
+++ src/module3/EarthquakeCityMap.java
@@ -42,6 +42,14 @@ public class EarthquakeCityMap extends PApplet {
     // Less than this threshold is a minor earthquake
     public static final float THRESHOLD_LIGHT = 4;
 
+    private final int GREEN = color(0, 255, 0);
+    private final int YELLOW = color(255, 255, 0);
+    private final int RED = color(255, 0, 0);
+
+    private static final float SMALL_R = 5.0f;
+    private static final float MED_R = 10.0f;
+    private static final float BIG_R = 15.0f;
+
     /**
      * This is where to find the local tiles, for working without an Internet connection
      */
@@ -49,6 +57,8 @@ public class EarthquakeCityMap extends PApplet {
 
     // The map
     private UnfoldingMap map;
+
+    // The legend
 
     //feed with magnitude 2.5+ Earthquakes
     private String earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
@@ -76,26 +86,27 @@ public class EarthquakeCityMap extends PApplet {
         //PointFeatures have a getLocation method
         List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
 
-        for (PointFeature eq :
-                earthquakes) {
-            markers.add(createMarker(eq));
-        }
 
         //TODO (Step 3): Add a loop here that calls createMarker (see below)
         // to create a new SimplePointMarker for each PointFeature in
         // earthquakes.  Then add each new SimplePointMarker to the
         // List markers (so that it will be added to the map in the line below)
 
+        for (PointFeature eq :
+                earthquakes) {
+            markers.add(createMarker(eq));
+        }
 
         // Add the markers to the map so that they are displayed
         map.addMarkers(markers);
     }
 
+
     /* createMarker: A suggested helper method that takes in an earthquake
      * feature and returns a SimplePointMarker for that earthquake
      *
      * In step 3 You can use this method as-is.  Call it from a loop in the
-     * setp method.
+     * setup method.
      *
      * TODO (Step 4): Add code to this method so that it adds the proper
      * styling to each marker based on the magnitude of the earthquake.
@@ -114,17 +125,22 @@ public class EarthquakeCityMap extends PApplet {
 
         // Here is an example of how to use Processing's color method to generate
         // an int that represents the color yellow.
-        int yellow = color(255, 255, 0);
-        int red = color(255, 0, 0);
-        int green = color(0, 255, 0);
+
 
         marker.setColor(color(0, 0, 0));
-        if (mag < THRESHOLD_LIGHT)
-            marker.setColor(green);
-        if (mag >= THRESHOLD_LIGHT && mag < THRESHOLD_MODERATE)
-            marker.setColor(yellow);
-        if (mag >= THRESHOLD_MODERATE)
-            marker.setColor(red);
+        if (mag < THRESHOLD_LIGHT) {
+            marker.setColor(GREEN);
+            marker.setRadius(SMALL_R);
+        }
+        if (mag >= THRESHOLD_LIGHT && mag < THRESHOLD_MODERATE) {
+            marker.setColor(YELLOW);
+            marker.setRadius(MED_R);
+        }
+        if (mag >= THRESHOLD_MODERATE) {
+            marker.setColor(RED);
+            marker.setRadius(BIG_R);
+
+        }
 
 
         // TODO (Step 4): Add code below to style the marker's size and color
@@ -143,7 +159,32 @@ public class EarthquakeCityMap extends PApplet {
     // helper method to draw key in GUI
     // TODO: Implement this method to draw the key
     private void addKey() {
+//        map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
+
         // Remember you can use Processing's graphics methods here
+        fill(255);
+        rect(20, 50, 160, 500);
+
+        fill(GREEN);
+        ellipse(40, 90, SMALL_R, SMALL_R);
+
+        fill(YELLOW);
+        ellipse(40, 110, MED_R, MED_R);
+
+        fill(RED);
+        ellipse(40, 130, BIG_R, BIG_R);
+
+        fill(0);
+        textSize(18);
+
+        text("Legend", 40, 70);
+
+        textSize(12);
+
+        text("Light", 60, 90 + SMALL_R / 2);
+        text("Moderate", 60, 110 + MED_R / 2);
+        text("Massive", 60, 130 + BIG_R / 2);
+
 
     }
 
