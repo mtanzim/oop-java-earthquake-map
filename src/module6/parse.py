b60d57838b4e06e83f52394495fdc7852f886f96
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-04-21 23:49:15 +0200

    visualization function

--- src/module6/parse.py
+++ src/module6/parse.py
@@ -1,6 +1,8 @@
 import xmltodict
 import pprint
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_mag(val):
@@ -13,9 +15,21 @@ def sort_function(a, b):
     return get_mag(a) - get_mag(b)
 
 
-with open('data/quiz2.atom') as fd:
-    doc = xmltodict.parse(fd.read())
-    titles = [v['title'] for v in doc['feed']['entry']]
-    titles.sort(key=get_mag)
-    for title in titles[-15:]:
-        print(title)
+if __name__ == "__main__":
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    NUM_ELEMS = 20
+
+    with open('data/quiz2.atom') as fd:
+        doc = xmltodict.parse(fd.read())
+        titles = [v['title'] for v in doc['feed']['entry']]
+        titles.sort(key=get_mag)
+        sorted_titles = titles[-NUM_ELEMS:]
+        for title in sorted_titles:
+            print(title)
+        y_pos = np.arange(len(sorted_titles))
+        mags = [get_mag(eq) for eq in sorted_titles]
+        ax.barh(y_pos, mags, align='center')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(titles)
+        plt.show()
